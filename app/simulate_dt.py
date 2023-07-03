060a0975a818977251c913637ba41c3ceac6917e
Nurseit Kamchyev <bncer.ml@gmail.com>
2023-07-04 00:34:52 +0600

    execute scheduler function in main

--- app/simulate_dt.py
+++ app/simulate_dt.py
@@ -1,20 +1,17 @@
-from datetime import datetime, timedelta
 import requests
+from datetime import datetime, timedelta
 
-from fastapi import Depends
-from sqlalchemy.orm import Session
 from sqlalchemy import func
 
-from app.database import SessionLocal
-from app import models
 from app.config import settings
+from app.schemas import CrimeBase
 
 
 def daterange(start_date, end_date):
     for n in range(1, int((end_date - start_date).days)+1):
         yield start_date + timedelta(n)
 
-def latest_date(db_sesion: Session):
+def add_data(models, db_sesion):
     db = db_sesion()
 
     date_time = db.query(models.Time.days).first()
@@ -28,15 +25,11 @@ def latest_date(db_sesion: Session):
     else:
         day_offset = day_diff - date_time[0]
         day_offset *= 3
-        print(day_offset)
         end_date = start_date + timedelta(days=day_offset)
+        print(date_time[0] - day_offset)
         for single_date in daterange(start_date, end_date):
             print(single_date)
-            print(date_time[0] - day_offset)            
             db.query(models.Time).\
                 filter(models.Time.id == 1).\
                 update({'days': date_time[0] - day_offset})
             db.commit()
-        
-
-latest_date(SessionLocal)
